a3f2a5d09b31050d252600c631d41ca981c45d61
Adam Grík <adam4grik@gmail.com>
2023-02-17 22:47:00 +0100

    test

---
+++ dbs_assignment/endpoints/hello.py
@@ -0,0 +1,57 @@
+from fastapi import APIRouter
+import asyncio
+
+from dbs_assignment.config import settings
+
+import psycopg2
+from psycopg2 import Error
+# from config import config
+
+router = APIRouter()
+
+# @router.get("/v1/status")
+# async def hello():
+#     return {
+#         'hello': settings.POSTGRES_USER
+#     }
+
+@router.get("/v1/status")
+async def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        # print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(user=settings.POSTGRES_USER,
+                                password=settings.POSTGRES_PASSWORD,
+                                host=settings.POSTGRES_SERVER,
+                                port=settings.POSTGRES_PORT,
+                                database=settings.POSTGRES_DB)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        # print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+
+        # close the communication with the PostgreSQL
+        cur.close()
+
+        return {
+            "version": db_version[0]
+        }
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+
+# if __name__ == '__main__':
+#     print(asyncio.run(connect()))
+
+
+
