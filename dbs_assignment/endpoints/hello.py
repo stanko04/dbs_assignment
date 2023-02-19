bc5f1cde97a4ee9d155e710c9168dbf00158641a
Adam Grík <adam4grik@gmail.com>
2023-02-19 19:58:31 +0100

    did some changes

--- dbs_assignment/endpoints/hello.py
+++ dbs_assignment/endpoints/hello.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter
-import asyncio
 
 from dbs_assignment.config import settings
 
@@ -9,11 +8,9 @@ router = APIRouter()
 
 @router.get("/v1/status")
 async def connect():
-    """ Connect to the PostgreSQL database server """
     conn = None
     try:
         # connect to the PostgreSQL server
-        # print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(user=settings.POSTGRES_USER,
                                 password=settings.POSTGRES_PASSWORD,
                                 host=settings.POSTGRES_SERVER,
@@ -23,14 +20,10 @@ async def connect():
         # create a cursor
         cur = conn.cursor()
 
-        # execute a statement
-        # print('PostgreSQL database version:')
         cur.execute('SELECT version()')
 
-        # display the PostgreSQL database server version
         db_version = cur.fetchone()
 
-        # close the communication with the PostgreSQL
         cur.close()
 
         return {
