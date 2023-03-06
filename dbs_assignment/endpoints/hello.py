f8409c74d78742e8e02c7789941db4d498a2e03c
Adam Grík <adam4grik@gmail.com>
2023-03-06 11:36:58 +0100

    add endpoint

--- dbs_assignment/endpoints/hello.py
+++ dbs_assignment/endpoints/hello.py
@@ -136,8 +136,8 @@ async def most_served_passengers(limit: int):
             'flights.flight_no, '
             'COUNT(ticket_flights) as counter '
         'FROM bookings.flights '
-        ' JOIN bookings.ticket_flights ON(flights.flight_id = ticket_flights.flight_id) '
-        ' WHERE(flights.flight_id = ticket_flights.flight_id) '
+        ' JOIN bookings.boarding_passes ON (flights.flight_id = boarding_passes.flight_id) '
+        ' WHERE (flights.flight_id = boarding_passes.flight_id) '
         ' GROUP BY flight_no '
         ' ORDER BY counter DESC '
         ' LIMIT %s', [limit])
